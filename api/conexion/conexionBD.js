53fe8330c4267014c4c210d5665a782d3a916659
Jose Limbert <jose.jl@gmail.com>
2022-05-23 22:27:31 -0400

    Coreccion conexion BD

--- api/conexion/conexionBD.js
+++ api/conexion/conexionBD.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
 const mysqlConection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234jose',
-    database: 'pruebaBD',
-    port: '3306'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bf0c5a31b08c6c',
+    password: 'f53e2b4b',
+    database: 'heroku_dc70b0c55510019?reconnect=true'
+        // port: '3306'
 })
+
 mysqlConection.connect((err) => {
     if (err) {
         console.log('Error conexion con la Base de Datos:', err);
