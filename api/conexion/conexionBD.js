b6b5b03d902d53db1f9e3991389fc99ffaeb8ad0
Jose Limbert <jose.jl@gmail.com>
2022-05-23 22:51:22 -0400

    Correcion puerto

--- api/conexion/conexionBD.js
+++ api/conexion/conexionBD.js
@@ -3,8 +3,8 @@ const mysqlConection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'bf0c5a31b08c6c',
     password: 'f53e2b4b',
-    database: 'heroku_dc70b0c55510019?reconnect=true'
-        // port: '3306'
+    database: 'heroku_dc70b0c55510019?reconnect=true',
+    port: '3306'
 })
 
 mysqlConection.connect((err) => {
