4010cd6377016879f57de37e8af08805aa265c2c
Jose Limbert <jose.jl@gmail.com>
2022-06-25 05:00:12 -0400

    correccion api twilo

--- config-sms.js
+++ config-sms.js
@@ -1,8 +1,5 @@
 module.exports = {
-    serviceID: "VA76f0482ee2acb951c2b8f393c34c910e",
-    // serviceID: process.env.serviceID,
-    accountSID: "AC805111d26862f3b24b4ece8f8a629c66",
-    // accountSID: process.env.accountSID,
-    authToken: "f30faa7850d7a268f8f871cdc4beedc1"
-        // authToken: process.env.authToken
+    serviceID: process.env.serviceID,
+    accountSID: process.env.accountSID,
+    authToken: process.env.authToken
 }
